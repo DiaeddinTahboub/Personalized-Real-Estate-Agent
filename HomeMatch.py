d6ec758e88846c8403ec0ef2352def125ec0eae0
Diaeddin Tahboub <diaeddintahboub@gmail.com>
2025-05-26 23:16:16 +0300

    code: prepared the workspace and installed all the dependencies/libraries.

---
+++ HomeMatch.py
@@ -0,0 +1,9 @@
+#A starter file for the HomeMatch application if you want to build your solution in a Python program instead of a notebook. 
+
+import os
+
+os.environ["OPENAI_API_KEY"] = "voc-1688312152126677393003667f71434296285.48009593"
+os.environ["OPENAI_API_BASE"] = "https://openai.vocareum.com/v1"
+
+from langchain.llms import OpenAI
+
